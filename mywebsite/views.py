72ebbecd3cfc6121a2e51cf22b2d3020b4861f42
GrayWizard12345 <muslim4ik14@gmail.com>
2023-05-13 10:17:41 +0500

    Views updated

--- mywebsite/views.py
+++ mywebsite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 # Create your views here.
-from .models import Project
+from .models import Project, Post, Comment
 
 
 def index(request):
@@ -19,3 +19,16 @@ def projects(request):
     projects = Project.objects.all()
     context = {'projects': projects}
     return render(request, 'projects.html', context)
+
+
+def blog_index(request):
+    posts = Post.objects.all().order_by('-post_date')
+    context = {'posts': posts}
+    return render(request, 'blog_index.html', context)
+
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post).order_by('-comment_date')
+    context = {'post': post, 'comments': comments}
+    return render(request, 'blog_detail.html', context)
